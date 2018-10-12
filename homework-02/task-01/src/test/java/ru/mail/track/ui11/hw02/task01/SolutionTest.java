044e6d27c0a23d14a67b0e16f581fabd3dc45028
Alexander Ivanovsky <alxiw@ya.ru>
2018-10-13 02:00:17 +0300

    Changed project structure, added homework-02 module with blanks

---
+++ homework-02/task-01/src/test/java/ru/mail/track/ui11/hw02/task01/SolutionTest.java
@@ -0,0 +1,10 @@
+package ru.mail.track.ui11.hw02.task01;
+
+import static org.junit.Assert.*;
+
+/**
+ * Тест-класс для Задачи №1 - Шифрование
+ */
+public class SolutionTest {
+
+}
---
+++ homework-02/task-02/src/test/java/ru/mail/track/ui11/hw02/task02/SolutionTest.java
@@ -0,0 +1,10 @@
+package ru.mail.track.ui11.hw02.task02;
+
+import static org.junit.Assert.*;
+
+/**
+ * Тест-класс для Задачи №2 - Слова и их цензура
+ */
+public class SolutionTest {
+
+}
